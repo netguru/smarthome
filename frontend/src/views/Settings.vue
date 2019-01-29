3ed63ccfe7915a95d52f2ab9f34131e975c5b094
Pawel Bochenski <pawel.bochenski@netguru.pl>
2019-01-29 09:14:22 +0100

    testing

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,60 +1,56 @@
 <template>
-  <div >
+  <div>
     <div class="loading" v-if="loading">Loading...</div>
     <div v-if="error" class="error">{{ error }}</div>
 
-    <v-container fluid fill-height>
-      <v-layout row wrap fill-height>
-        
-        <v-flex xs12 md4 v-if="content!==null">
-          <v-card>
-            <v-card-title class="dark" @click="deselect()">
-              <div>
-                Sensors
-                </div>
-              <v-spacer></v-spacer>
-              <AddSensorDialog @refresh='fetchData()' />
-            </v-card-title>
-            <v-list>
-              <v-list-tile v-for="item in content" :key="item.id" @click="onSelect(item)">
-                <v-list-tile-content>
-                  <v-list-tile-title>#{{item.id}} {{item.name}} ({{item.topic}})</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </v-list>
-          </v-card>
+    <v-container fluid fill-height class="pink">
+        <v-layout row>
+          <v-flex xs12 md4 v-if="content!==null">
+            <v-card>
+              <v-card-title class="dark" @click="deselect()">
+                <div>Sensors</div>
+                <v-spacer></v-spacer>
+                <AddSensorDialog @refresh="fetchData()"/>
+              </v-card-title>
+              <v-list>
+                <v-list-tile v-for="item in content" :key="item.id" @click="onSelect(item)">
+                  <v-list-tile-content>
+                    <v-list-tile-title>#{{item.id}} {{item.name}} ({{item.topic}})</v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+          <v-divider vertical/>
+          <v-flex md8 grow class="hidden-sm-and-down">
+            <SensorEditCard
+              v-if="editSensor"
+              title="Edit Sensor"
+              :sensor="editSensor"
+              deleteButton
+            />
 
-        </v-flex >
-
-        <v-flex md8 grow class="pl-2 hidden-sm-and-down">
-          <SensorEditCard v-if="editSensor"
-            title="Edit Sensor"
-            :sensor='editSensor'
-            deleteButton
-          />
-
-          <div v-if="editSensor===null">
-            click + to add sensor or select sensor to edit
-          </div>
-        </v-flex>
-
-      </v-layout>
+            <v-card
+              v-if="editSensor===null"
+              height="100%"
+              class="justify-center"
+            >click + to add sensor or select sensor to edit</v-card>
+          </v-flex>
+        </v-layout>
     </v-container>
-
-
   </div>
 </template>
 
 <script >
-import axios from 'axios';
-import AddSensorDialog from '@/components/AddSensorDialog.vue';
-import SensorEditCard from '@/components/SensorEditCard.vue';
+import axios from "axios";
+import AddSensorDialog from "@/components/AddSensorDialog.vue";
+import SensorEditCard from "@/components/SensorEditCard.vue";
 
 export default {
-  name: 'Settings',
+  name: "Settings",
   components: {
     AddSensorDialog,
-    SensorEditCard,
+    SensorEditCard
   },
   data() {
     return {
@@ -62,14 +58,14 @@ export default {
       editSensor: null,
       loading: false,
       content: null,
-      error: null,
+      error: null
     };
   },
   created() {
     this.fetchData();
   },
   watch: {
-    $route: 'fetchData',
+    $route: "fetchData"
   },
   methods: {
     fetchData() {
@@ -77,22 +73,22 @@ export default {
       this.loading = true;
       axios
         .get(`${this.$store.state.host}//get_sensors_all`)
-        .then((response) => {
+        .then(response => {
           this.loading = false;
           this.content = response.data;
         })
-        .catch((err) => {
+        .catch(err => {
           this.loading = false;
           this.error = err.toString();
         });
     },
     onSelect(item) {
-       this.editSensor = item;
+      this.editSensor = item;
     },
     deselect() {
       this.editSensor = null;
-    },
-  },
+    }
+  }
 };
 </script>
 
