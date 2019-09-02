e27e1924ebd317432c6276455eb4694eacfe3783
Pawel Bochenski <pawel.bochenski@netguru.pl>
2019-09-02 22:31:30 +0200

    added navigation bar

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import './App.scss'
+import React , {Component} from 'react';
+import './App.scss';
+import classNames from 'classnames'
 
-function App() {
+class App extends Component{
+  state = {
+    navMenuActive: false,
+  }
+  handleHamburgerClick = (e) => {
+    e.preventDefault();
+    this.setState({...this.state, navMenuActive: !this.state.navMenuActive});
+  }
+
+  render(){
   return (
-    <section class="section">
-      <div class="columns">
-        <div class="column">
-          First column
-  </div>
-        <div class="column">
-          Second column
-  </div>
-        <div class="column">
-          Third column
-  </div>
-        <div class="column">
-          Fourth column
-  </div>
+    <div>
+      <nav class="navbar is-fixed-top" role="navigation">
+      <div class="navbar-brand">
+        <div role="button" 
+        className={classNames({"navbar-burger": true, "is-active": this.state.navMenuActive })}
+           onClick={this.handleHamburgerClick}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+         </div>
       </div>
-    </section>
+        <div className={classNames({"navbar-menu": true, "is-active": this.state.navMenuActive })} >
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Dashboard
+            </a>
+            <a class="navbar-item">
+              Sensors
+            </a>
+          </div>
+
+          <div class="navbar-end">
+          <a class="navbar-item">
+              Settings
+            </a>
+          </div>
+        </div>
+      </nav>
+      
+    </div>
   );
+  }
 }
 
 export default App;
