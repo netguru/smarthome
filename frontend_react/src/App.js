ac7da21a3d06dd70e2418b218ec5c87d2eafe03f
Pawel Bochenski <pawel.bochenski@netguru.pl>
2019-09-02 21:18:19 +0200

    started work on frontend in react

---
+++ frontend_react/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.scss'
+
+function App() {
+  return (
+    <section class="section">
+      <div class="columns">
+        <div class="column">
+          First column
+  </div>
+        <div class="column">
+          Second column
+  </div>
+        <div class="column">
+          Third column
+  </div>
+        <div class="column">
+          Fourth column
+  </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
